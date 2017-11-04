b2d20a01abb7e8a018b3662e77d7f985d46b961a
onkyoworm <996570982@qq.com>
2017-11-04 18:46:40 +0800

    50%

--- FreeIP.py
+++ FreeIP.py
@@ -2,6 +2,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import urllib2
 
 
 url1 = 'http://www.xicidaili.com/'								#西刺
@@ -9,8 +10,8 @@ url2 = 'http://www.kuaidaili.com/free/'							#快代理
 url3 = 'http://www.httpdaili.com/mfdl/'							#httpdaili
 url4 = 'http://www.66ip.cn/'									#66ip
 
+#simple of each website headers
 header_xici = {
-
 	'Host': 'www.xicidaili.com',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0',
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -20,10 +21,19 @@ header_xici = {
 	'Connection': 'keep-alive',
 	'Upgrade-Insecure-Requests': '1',
 	'If-None-Match': 'W/"cb19fedc96a8d69fbd9a357a170afba0"',
-	'Cache-Control': 'max-age=0'
+	'Cache-Control': 'max-age=0'}
 
-}
-
+header_httpdaili = {
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Language': 'zh,zh-CN;q=0.8,en-US;q=0.5,en;q=0.3',
+	'Accept-Encoding': 'gzip, deflate',
+	'Cookie': 'UM_distinctid=15f7c0cae40449-0f196bd5a175638-4c322f7c-1fa400-15f7c0cae415b0; CNZZDATA4146503=cnzz_eid%3D1168875105-1509614725-https%253A%252F%252Fwww.baidu.com%252F%26ntime%3D1509698760',
+	'Connection': 'keep-alive',
+	'Upgrade-Insecure-Requests': '1',
+	'If-Modified-Since': 'Mon, 23 Oct 2017 01:13:45 GMT',
+	'If-None-Match': "4a3d552c9c4bd31:a048",
+	'Cache-Control': 'max-age=0'}
 
 def xici(url1):
 	surf = requests.get(url=url1, headers=header_xici)
@@ -56,16 +66,23 @@ def xici(url1):
 	# 	print cc
 	print get_IP[2]
 
-def httpdaili(url2):
+def httpdaili(url3):
 
+	#for avoid banned we store the raw html file
+
+	surf = requests.get(url=url2, headers=header_httpdaili)
+	surf = urllib2.urlopen(url3).read()
+	with open('123.html', 'w+') as f:
+		f.write(surf)
+	f.close()
 	#divide into three parts
-	analyze = BeautifulSoup(httpdaili_text, 'lxml')
+	analyze = BeautifulSoup(surf, 'lxml')
 	
 	#part1--Chinese's http proxy
 	infor_list1 = list()										#include the name and the ip's detail
 
 	#locate the informations
-	part1 = analyze.find('li', attrs={'style': re.compile('position: absolute; left: 10.5')})
+	part1 = analyze.find('li', attrs={'style': re.compile('position: absolute; left: 10.5px; top: 0px;')})
 	for i in part1.table.children:
 		if i != '\n':
 			infor_soup = BeautifulSoup(str(i), 'lxml')
@@ -88,13 +105,54 @@ def httpdaili(url2):
 
 	#######################################################################################################		
 
-	#part2--foreign htt proxy
-	infor_list1 = list()
+	#part2--foreign http proxy
+	infor_list2 = list()
 
 	#locate the informations
 	part2 = analyze.find('li', attrs={'style': re.compile('position: absolute; left: 410.5px; top: 0px;')})
-	print part2
-if __name__ == '__main__': 
+	for i in part2.table.children:
+		if i != '\n':
+			infor_soup2 = BeautifulSoup(str(i), 'lxml')
+			#add infor
+			infor_list2.append(infor_soup2)
+	length2 = len(infor_list2)
+	for i in range(length2):
+		for j in infor_list2[i]:
+			#print j.get_text()
+			pass
+	#print infor_list2[0].get_text()
+	#infor_list2[0] is the format of name
+	#others is ip's detail
+	for i in range(1, length1):
+		for j in infor_list2[i]:
+			#print j.get_text()
+			pass
 
+	#######################################################################################################		
+
+	#part3-- https proxy	
+	infor_list3 = list()
+
+	#locate the informations
+	part3 = analyze.find('li', attrs={'style': re.compile('position: absolute; left: 810.5px; top: 0px;')})
+	for i in part3.table.children:
+		if i != '\n':
+			infor_soup3 = BeautifulSoup(str(i), 'lxml')
+			#add infor
+			infor_list3.append(infor_soup3)
+	length3 = len(infor_list3)
+	for i in range(length3):
+		for j in infor_list3[i]:
+			#print j.get_text()
+			pass
+	#print infor_list3[0].get_text()
+	#infor_list3[0] is the format of name
+	#others is ip's detail
+	for i in range(1, length1):
+		for j in infor_list3[i]:
+			#print j.get_text()
+			pass
+			
+if __name__ == '__main__':
 	#xici(url1)
-	httpdaili(url2)
+	httpdaili(url3)
